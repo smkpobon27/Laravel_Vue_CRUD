5db06c3da8ad9be3224f961dff2efad0eb84d817
MIS-SE-S M Kamruzzaman <smkamruzzaman@aci-bd.com>
2022-02-06 18:02:07 +0600

    initial commit

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'total' => $this->faker->randomFloat($nbMaxDecimals = 2, $min = 100.00, $max = 1000.00)
+
+        ];
+    }
+}
---
+++ resources/js/components/CustomerComponent.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="customer">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ 'Customer' }}</div>
+
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped small table-sm">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Address</th>
+                                <th scope="col">Total</th>
+                                <th scope="col" class="text-center">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(customer, index) in customers" :key="customer.id">
+                                    <td>{{index + 1}}</td>
+                                    <td>{{customer.name}}</td>
+                                    <td>{{customer.email}}</td>
+                                    <td>{{customer.phone}}</td>
+                                    <td>{{customer.address}}</td>
+                                    <td>{{customer.total}}</td>
+                                    <td class="text-center">
+                                        <button type="button" class="btn btn-info btn-sm"><i class="fas fa-eye"></i></button>
+                                        <button type="button" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></button>
+                                        <button type="button" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <vue-progress-bar></vue-progress-bar>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                customers : [],
+            }
+        },
+        mounted() {
+            console.log('Component mounted.');
+            this.getData();
+            
+        },
+
+        methods:{
+            getData : function(){
+                 this.$Progress.start();
+                axios.get('/api/customers')
+                    .then(response => {
+                        this.customers = response.data.data;
+                        this.$Progress.finish();
+                        // console.log(response);
+                        // console.log(this.customers);
+                    })
+                    .catch(e => {
+                        console.log(e); 
+                        this.$Progress.fail();   
+                    })
+            }
+        }
+    }
+</script>
